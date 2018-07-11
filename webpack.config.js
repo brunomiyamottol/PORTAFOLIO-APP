9a6d4cd64d3f10b0c8a5e89c34e1ee690af1080a
brunomiyamotto <brunomluque@gmail.com>
2018-07-11 00:05:15 -0500

    Setup prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path'); 
 
-module.exports = {
+
+
+module.exports = (env) => {
+	const isProduction = env === 'production'; 
+	
+	
+	return{
 	entry:'./src/app.js', 
 	output:{
 		path:path.join(__dirname, 'public'), 
@@ -20,9 +26,10 @@ module.exports = {
 			]
 		}]
 	}, 
-	devtool:'cheap-eval-source-map', 
+	devtool: isProduction ? 'source-map' : 'cheap-eval-source-map', 
 	devServer:{
 		contentBase:path.join(__dirname, 'public'), 
 		historyApiFallback:true 
+		}
 	}
 }; 
