66fb4555805ffc9c5972d0bf552b3e75a9d968ce
brunomiyamotto <brunomluque@gmail.com>
2018-07-16 00:09:55 -0500

    Directory Structure and Image Size

--- client/src/components/CarouselPage.js
+++ client/src/components/CarouselPage.js
@@ -1,5 +1,16 @@
-import React, { Component } from 'react';
-import { Carousel, CarouselCaption, CarouselControl, CarouselInner, CarouselItem, CarouselIndicators, CarouselIndicator, View, Mask, Container } from 'mdbreact';
+import React, { Component } from "react";
+import {
+  Carousel,
+  CarouselCaption,
+  CarouselControl,
+  CarouselInner,
+  CarouselItem,
+  CarouselIndicators,
+  CarouselIndicator,
+  View,
+  Mask,
+  Container
+} from "mdbreact";
 
 export default class CarouselPage extends Component {
   constructor(props) {
@@ -14,19 +25,27 @@ export default class CarouselPage extends Component {
 
   next() {
     let nextItem = this.state.activeItem + 1;
-    if(nextItem > this.state.maxLength) {
-      this.setState({ activeItem: 1 });
+    if (nextItem > this.state.maxLength) {
+      this.setState({
+        activeItem: 1
+      });
     } else {
-      this.setState({ activeItem: nextItem });
+      this.setState({
+        activeItem: nextItem
+      });
     }
   }
 
   prev() {
     let prevItem = this.state.activeItem - 1;
-    if(prevItem < 1) {
-      this.setState({ activeItem: this.state.maxLength });
+    if (prevItem < 1) {
+      this.setState({
+        activeItem: this.state.maxLength
+      });
     } else {
-      this.setState({ activeItem: prevItem });
+      this.setState({
+        activeItem: prevItem
+      });
     }
   }
 
@@ -38,77 +57,156 @@ export default class CarouselPage extends Component {
     }
   }
 
-  render(){
+  render() {
     const { activeItem } = this.state;
-    return(
+    return (
       <Container>
-        <h4 className="mt-5 mb-2"></h4>
+        <h4 className="mt-5 mb-2"> </h4>{" "}
         <Carousel
           activeItem={this.state.activeItem}
           next={this.next}
-          className="z-depth-1">
+          className="z-depth-1"
+        >
           <CarouselInner>
-            <CarouselItem itemId="1">
+            {" "}<CarouselItem itemId="1">
               <View>
-                <img className="d-block w-100" src="/images/bml1.jpg" alt="First slide" />
-                <Mask overlay="black-light"></Mask>
-              </View>
+                <img
+                  className="d-block w-100"
+                  src="/images/bml1.jpeg"
+                  alt="First slide"
+                />
+                <Mask overlay="black-light"> </Mask>{" "}
+              </View>{" "}
               <CarouselCaption>
-                <h3 className="h3-responsive">Bruno Miyamoto</h3>
-                <h4>"All men dream, but not equally. Those who dream by night in the dusty recesses of their minds, wake in the day to find that it was vanity: but the dreamers of the day are dangerous men, for they may act on their dreams with open eyes, to make them possible."</h4>
-              </CarouselCaption>
-            </CarouselItem>
+                <h3 className="h3-responsive"> Bruno Miyamoto </h3>{" "}
+                <h4>
+                  {" "}"All men dream, but not equally. Those who dream by night
+                  in the dusty recesses of their minds, wake in the day to find
+                  that it was vanity: but the dreamers of the day are dangerous
+                  men, for they may act on their dreams with open eyes, to make
+                  them possible."{" "}
+                </h4>{" "}
+              </CarouselCaption>{" "}
+            </CarouselItem>{" "}
             <CarouselItem itemId="2">
               <View>
-                <img className="d-block w-100" src="/images/blur-blurred-background.jpg" alt="Second slide" />
-                <Mask overlay="black-strong"></Mask>
-              </View>
+                <img
+                  className="d-block w-100"
+                  src="/images/blur-blurred-background.jpeg"
+                  alt="Second slide"
+                />
+                <Mask overlay="black-strong"> </Mask>{" "}
+              </View>{" "}
               <CarouselCaption>
-                <h3 className="h3-responsive">"Don't Take Yourself Too Seriously"</h3>
-                <p></p>
-              </CarouselCaption>
-            </CarouselItem>
+                <h3 className="h3-responsive">
+                  {" "}"Don't Take Yourself Too Seriously"{" "}
+                </h3>{" "}
+                <p> </p>{" "}
+              </CarouselCaption>{" "}
+            </CarouselItem>{" "}
             <CarouselItem itemId="3">
               <View>
-                <img className="d-block w-100" src="/images/action-adrenaline-adventure.jpg" alt="Third slide" />
-                <Mask overlay="black-slight"></Mask>
-              </View>
+                <img
+                  className="d-block w-100"
+                  src="/images/action-adrenaline-adventure.jpeg"
+                  alt="Third slide"
+                />
+                <Mask overlay="black-slight"> </Mask>{" "}
+              </View>{" "}
               <CarouselCaption>
-                <h3 className="h3-responsive">Be Inquisitive, Empathetic, Systematic and Relentless</h3>
-                <p></p>
-              </CarouselCaption>
-            </CarouselItem>
+                <h3 className="h3-responsive">
+                  {" "}Be Inquisitive, Empathetic, Systematic and Relentless{" "}
+                </h3>{" "}
+                <p> </p>{" "}
+              </CarouselCaption>{" "}
+            </CarouselItem>{" "}
             <CarouselItem itemId="4">
               <View>
-                <img className="d-block w-100" src="/images/astronomy-cosmos-dark.jpg" alt="Mattonit's item" />
-                <Mask overlay="black-light"></Mask>
-              </View>
+                <img
+                  className="d-block w-100"
+                  src="/images/astronomy-cosmos-dark.jpeg"
+                  alt="Mattonit's item"
+                />
+                <Mask overlay="black-light"> </Mask>{" "}
+              </View>{" "}
               <CarouselCaption>
-                <h3 className="h3-responsive">Pursue you're Passions, Not the Money</h3>
-                <p></p>
-              </CarouselCaption>
-						</CarouselItem>
-						<CarouselItem itemId="5">
-						<View>
-							<img className="d-block w-100" src="/images/legs-outdoors-person.jpg" alt="Mattonit's item" />
-							<Mask overlay="black-light"></Mask>
-						</View>
-						<CarouselCaption>
-							<h3 className="h3-responsive">Believe in Change</h3>
-							<p></p>
-						</CarouselCaption>
-					</CarouselItem>
-          </CarouselInner>
-          <CarouselControl direction="prev" role="button" onClick={() => { this.prev(); }} />
-          <CarouselControl direction="next" role="button" onClick={() => { this.next(); }} />
+                <h3 className="h3-responsive">
+                  {" "}Pursue you 're Passions, Not the Money
+                </h3>{" "}
+                <p> </p>{" "}
+              </CarouselCaption>{" "}
+            </CarouselItem>{" "}
+            <CarouselItem itemId="5">
+              <View>
+                <img
+                  className="d-block w-100"
+                  src="/images/legs-outdoors-person.jpeg"
+                  alt="Mattonit's item"
+                />
+                <Mask overlay="black-light"> </Mask>{" "}
+              </View>{" "}
+              <CarouselCaption>
+                <h3 className="h3-responsive"> Believe in Change </h3> <p> </p>{" "}
+              </CarouselCaption>{" "}
+            </CarouselItem>{" "}
+          </CarouselInner>{" "}
+          <CarouselControl
+            direction="prev"
+            role="button"
+            onClick={() => {
+              this.prev();
+            }}
+          />{" "}
+          <CarouselControl
+            direction="next"
+            role="button"
+            onClick={() => {
+              this.next();
+            }}
+          />{" "}
           <CarouselIndicators>
-            <CarouselIndicator active={activeItem === 1 ? true : false} onClick={() => { this.goToIndex(1); }}></CarouselIndicator>
-            <CarouselIndicator active={activeItem === 2 ? true : false} onClick={() => { this.goToIndex(2); }}></CarouselIndicator>
-            <CarouselIndicator active={activeItem === 3 ? true : false} onClick={() => { this.goToIndex(3); }}></CarouselIndicator>
-						<CarouselIndicator active={activeItem === 4 ? true : false} onClick={() => { this.goToIndex(4); }}></CarouselIndicator>
-						<CarouselIndicator active={activeItem === 5 ? true : false} onClick={() => { this.goToIndex(5); }}></CarouselIndicator>
-          </CarouselIndicators>
-        </Carousel>
+            <CarouselIndicator
+              active={activeItem === 1 ? true : false}
+              onClick={() => {
+                this.goToIndex(1);
+              }}
+            >
+              {" "}
+            </CarouselIndicator>{" "}
+            <CarouselIndicator
+              active={activeItem === 2 ? true : false}
+              onClick={() => {
+                this.goToIndex(2);
+              }}
+            >
+              {" "}
+            </CarouselIndicator>{" "}
+            <CarouselIndicator
+              active={activeItem === 3 ? true : false}
+              onClick={() => {
+                this.goToIndex(3);
+              }}
+            >
+              {" "}
+            </CarouselIndicator>{" "}
+            <CarouselIndicator
+              active={activeItem === 4 ? true : false}
+              onClick={() => {
+                this.goToIndex(4);
+              }}
+            >
+              {" "}
+            </CarouselIndicator>{" "}
+            <CarouselIndicator
+              active={activeItem === 5 ? true : false}
+              onClick={() => {
+                this.goToIndex(5);
+              }}
+            >
+              {" "}
+            </CarouselIndicator>{" "}
+          </CarouselIndicators>{" "}
+        </Carousel>{" "}
       </Container>
     );
   }
