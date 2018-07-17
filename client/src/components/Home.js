31fd01172149051f02d78aa72505013ea6a80146
brunomiyamotto <brunomluque@gmail.com>
2018-07-16 20:01:50 -0500

    Added Client-Server Structure

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -15,26 +15,22 @@ import '../styles/styles.scss';
 
 export default class Home extends Component {
 	render() {
-		return ( < div >
-			<
-			div className = "container" >
-			<
-			CarouselPage / >
-			<
-			Jumbotron >
-			<
-			Grid >
-			<
-			div className = "container" >
-			<
-			p > < strong > Continuously develop the passion
-			for learning, understanding and processing information that influences how we perceive and interact with reality < /strong></p >
-			<
-			/div> < /
-			Grid > <
-			/Jumbotron> < /
-			div > <
-			/div>
+		return (
+			<div>
+				<div className="container" >
+					<CarouselPage />
+					<Jumbotron >
+						<Grid >
+							<div className="container" >
+								<p>
+									<strong> Continuously develop the passion for learning, understanding and processing information that influences how we perceive and interact with reality </strong>
+								</p>
+							</div>
+						</Grid>
+					</Jumbotron>
+
+				</div>
+			</div>
 		);
 	}
 }
